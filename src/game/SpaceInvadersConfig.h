d3fc4b73b441e0c50603dfbacb37a99e6d9b3f4d
mat127 <p3tr.matousek@gmail.com>
2020-04-28 08:51:17 +0200

    # formating fixed

--- src/game/SpaceInvadersConfig.h
+++ src/game/SpaceInvadersConfig.h
@@ -4,10 +4,10 @@
 
 #define SIG_PLAYER_LIVES_COUNT		3
 
-#define SIG_SHIP_POSITION_Y			550
-#define SIG_SHIP_SKIN_FILENAME		"gfx/Big Invader.png"
-#define SIG_SHIP_NORMAL_SIZE		30
-#define SIG_SHIP_STEP_SIZE			7
+#define SIG_SHIP_POSITION_Y				550
+#define SIG_SHIP_SKIN_FILENAME			"gfx/Big Invader.png"
+#define SIG_SHIP_NORMAL_SIZE			30
+#define SIG_SHIP_STEP_SIZE				7
 #define SIG_SHIP_GUN_LOADING_INTERVAL	15
 #define SIG_SHIP_GUN_SHOT_DIRECTION		0,-4
 
@@ -18,6 +18,5 @@
 #define SIG_BULLET_SIZE				10
 #define SIG_BULLET_SPEED			4
 #define SIG_BULLET_ROTATION_SPEED	.1f
-
 
 #define SIG_HIGH_SCORE_FILENAME		"hs.bin"
